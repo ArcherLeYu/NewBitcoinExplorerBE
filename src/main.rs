760932ec0fc59908225bbb1712b29c94726de16b
Le Yu <ley@uoregon.edu>
2024-10-05 18:27:01 -0400

    part实现

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-// main.rs
 mod config;
 mod rpc_client;
 mod db;
 mod models;
 mod utils;
 mod errors;
-mod retryable;
 mod api;
 
 use env_logger;
-use actix_web::{HttpResponse, Responder};
-use actix_web::{web, App, HttpServer}; // 导入其他需要的 Actix 组件
+use actix_web::{web, App, HttpServer}; // 导入 Actix 组件
 use crate::errors::AppError; // 导入 AppError
 use tokio::time::{self, Duration};
-use std::sync::Arc;
-use actix_web::middleware::Logger;
+use api::handlers::get_blockchain_info; // 导入 get_blockchain_info 函数
+use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> Result<(), AppError> {
@@ -25,20 +22,21 @@ async fn main() -> Result<(), AppError> {
     // 创建配置对象
     let config = config::Config::new();
 
-    // 创建数据库连接池
-    let database = Arc::new(db::Database::new(&config.db_connection_string)?);
-
-    // 使用 Tokio 并发来定时从 Bitcoin Core 获取区块链数据
-    let database_clone = Arc::clone(&database);
+    // 任务 1：定时从 Bitcoin Core 获取区块链数据
+    // 直接创建数据库连接池实例并在每个循环中使用它
+    let config_clone = config.clone();  // 克隆配置以供异步任务使用
     tokio::spawn(async move {
         let mut interval = time::interval(Duration::from_secs(600)); // 每600秒（10分钟）运行一次
 
         loop {
             interval.tick().await; // 每隔10分钟触发一次
-            match rpc_client::get_blockchain_info(&config).await {
+            match rpc_client::get_blockchain_info(&config_clone).await {
                 Ok(blockchain_info) => {
+                    // 创建一个新的数据库连接池
+                    let database = db::Database::new(&config_clone.db_connection_string).unwrap();
+
                     // 检查区块哈希是否存在
-                    let hash_exists = database_clone
+                    let hash_exists = database
                         .check_block_hash_exists(&blockchain_info.bestblockhash)
                         .unwrap_or_else(|e| {
                             eprintln!("Failed to check block hash existence: {:?}", e);
@@ -48,11 +46,12 @@ async fn main() -> Result<(), AppError> {
                     if !hash_exists {
                         // 如果不存在，插入新的区块信息
                         println!("新区块链hash {:?}", blockchain_info.bestblockhash);
-                        if let Err(e) = database_clone.insert_block_info(&blockchain_info) {
+                        if let Err(e) = database.insert_block_info(&blockchain_info) {
                             eprintln!("Failed to insert block info: {:?}", e);
                         }
                     } else {
                         // 如果存在，可以选择打印信息或执行其他逻辑
+                        println!("No update");
                     }
                 }
                 Err(e) => {
@@ -62,24 +61,19 @@ async fn main() -> Result<(), AppError> {
         }
     });
 
-    // 使用 Actix Web 的运行时来启动 HTTP 服务器
-    println!("Starting HTTP server on http://127.0.0.1:8081...");
-    HttpServer::new(move || {
-        let app = App::new()
-            .wrap(Logger::default()) // 添加日志记录中间件
-            .app_data(web::Data::new(Arc::clone(&database)))
-            .route("/", web::get().to(root_handler)) // 处理根路径请求
-            .route("/blockchain-info", web::get().to(api::handlers::get_blockchain_info));
+    // 任务 2：提供 HTTP API 服务器，供前端获取最新的 blockchain 信息
+    let database = db::Database::new(&config.db_connection_string)?;
+    let database_data = web::Data::new(database);
 
-        app
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Cors::permissive()) // 添加 CORS 中间件，允许所有跨域请求
+            .app_data(database_data.clone()) // 将包装好的数据库对象传递给应用程序
+            .route("/blockchain-info", web::get().to(get_blockchain_info))
     })
         .bind("127.0.0.1:8081")?
         .run()
         .await?;
 
-
     Ok(())
-}
-async fn root_handler() -> impl Responder {
-    HttpResponse::Ok().body("Welcome to Bitcoin Explorer API. Use /blockchain-info to get the latest blockchain information.")
 }
