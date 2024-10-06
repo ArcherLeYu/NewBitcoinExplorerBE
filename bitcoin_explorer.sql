ae6a11715a56f05d1f9c2d40e65fa723c0d7b4f0
Le Yu <ley@uoregon.edu>
2024-10-05 21:07:14 -0400

    readme 和 数据库修理。

--- bitcoin_explorer.sql
+++ bitcoin_explorer.sql
@@ -1,19 +1,19 @@
+
 CREATE TABLE Blocks (
-                        automatic_pruning BOOLEAN NOT NULL DEFAULT false,
-                        block_hash VARCHAR(64) PRIMARY KEY,
+                        automatic_pruning TINYINT(1) NOT NULL DEFAULT 0,
+                        bestblockhash VARCHAR(64) PRIMARY KEY,
                         blocks BIGINT,
                         chain VARCHAR(32),
                         chainwork VARCHAR(128),
                         difficulty DOUBLE,
                         headers BIGINT,
-                        initialblockdownload BOOLEAN,
+                        initialblockdownload TINYINT(1),
                         mediantime BIGINT,
                         prune_target_size BIGINT,
-                        pruned BOOLEAN,
+                        pruned TINYINT(1),
                         pruneheight BIGINT,
                         size_on_disk BIGINT,
                         time BIGINT,
                         verificationprogress DOUBLE,
                         warnings TEXT
 );
-
